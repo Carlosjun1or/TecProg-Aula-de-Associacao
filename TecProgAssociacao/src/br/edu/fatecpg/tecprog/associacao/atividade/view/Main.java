9ea2eeb915247e27b71c1cc26dfebc7582647f9c
Carlos <crdantasjr@gmail.com>
2026-05-13 12:16:49 -0300

    feat: add demonstração de clientes, funcionarios e folha salarial no Main
    atualiza Main para demonstrar funcionalidades da atividade
    - registra 3 clientes com nome, email e mensalidade
    - adiciona 3 funcionarios via adicionarFuncionario com salario
    - exibe dados da empresa, lista de clientes e funcionarios
    - exibe folha salarial total calculada

--- TecProgAssociacao/src/br/edu/fatecpg/tecprog/associacao/atividade/view/Main.java
+++ TecProgAssociacao/src/br/edu/fatecpg/tecprog/associacao/atividade/view/Main.java
@@ -5,15 +5,23 @@ import br.edu.fatecpg.tecprog.associacao.atividade.model.*;
 public class Main {
 	public static void main(String[] args) {
 
-		Cliente c1 = new Cliente("Carlos", "crdantasjr@gmail.com", 254.20);
 		Empresa emp1 = new Empresa("Fatec", "Educação", "13:10 - 18:30");
-		Funcionario func1 = new Funcionario("Carolina", "Chefe", "Tecnologia");
+
+		emp1.registrarCliente(0, new Cliente("Carlos", "crdantasjr@gmail.com", 254.20));
+		emp1.registrarCliente(1, new Cliente("Carolina", "carolina@email.com", 300.00));
+		emp1.registrarCliente(2, new Cliente("Rubia", "rubia@email.com", 180.50));
+
+		emp1.adicionarFuncionario("Carolina", "Chefe", 7000.00);
+		emp1.adicionarFuncionario("Danilo", "Analista", 4500.00);
+		emp1.adicionarFuncionario("Pedro", "Desenvolvedor", 5200.00);
+
 		System.out.println(emp1);
 
-		emp1.registrarCliente(0, c1);
-		emp1.registrarCliente(1, c1);
-		emp1.registrarCliente(2, c1);
-		
+		System.out.println("\n--- Clientes ---");
 		System.out.println(emp1.apresentarClientes());
+
+		emp1.exibirFuncionarios();
+		System.out.println("");
+		emp1.calcularFolhaSalarial();
 	}
 }
